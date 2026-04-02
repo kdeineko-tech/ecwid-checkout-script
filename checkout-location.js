2c202453216cfb5c71709a05fd2d7f94da193d94
Hryhorii Haponiuk <hhaponiuk@quitcode.com>
2026-04-02 21:16:57 +0300

    Update checkout-location.js

--- checkout-location.js
+++ checkout-location.js
@@ -1,14 +1,36 @@
+const STATE_FIELD_LABEL = "Choose State";
+const CITY_FIELD_LABEL = "Choose City";
+
+const CITY_MAP = {
+  "California": ["Los Angeles", "San Diego", "San Jose", "Sacramento"],
+  "Texas": ["Houston", "Dallas", "Austin", "San Antonio"],
+  "Florida": ["Miami", "Orlando", "Tampa", "Jacksonville"],
+  "New York": ["New York", "Buffalo", "Albany", "Rochester"]
+};
+
 Ecwid.OnAPILoaded.add(function () {
   window.ec = window.ec || {};
   ec.order = ec.order || {};
   ec.order.extraFields = ec.order.extraFields || {};
 
-  ec.order.extraFields.delivery_comment = {
-    title: 'Delivery comment',
-    type: 'text',
-    textPlaceholder: 'Enter comment',
-    required: false,
-    checkoutDisplaySection: 'shipping_address'
+  ec.order.extraFields.custom_state = {
+    title: STATE_FIELD_LABEL,
+    type: "select",
+    required: true,
+    checkoutDisplaySection: "shipping_address",
+    options: Object.keys(CITY_MAP).map(function (state) {
+      return { title: state };
+    })
+  };
+
+  ec.order.extraFields.custom_city = {
+    title: CITY_FIELD_LABEL,
+    type: "select",
+    required: true,
+    checkoutDisplaySection: "shipping_address",
+    options: CITY_MAP["Texas"].map(function (city) {
+      return { title: city };
+    })
   };
 
   window.Ecwid && Ecwid.refreshConfig();
